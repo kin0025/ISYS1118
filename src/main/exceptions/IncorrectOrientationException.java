7dd2b66c2eada65b12cd6abaed3ba3886bf61380
Alex Kinross-Smith <s3603437@student.rmit.edu.au>
2017-08-16 11:32:41 +1000

    Forgot to include another exception in previous commit

---
+++ src/main/exceptions/IncorrectOrientationException.java
@@ -0,0 +1,8 @@
+package main.exceptions;
+public class IncorrectOrientationException extends Exception{
+    public IncorrectOrientationException() {
+    }
+    public IncorrectOrientationException(String e){
+        super(e);
+    }
+}

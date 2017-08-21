d6c393a8d50afcff947cb6525cfa95b02f32a1df
Alex Kinross-Smith <s3603437@student.rmit.edu.au>
2017-08-21 18:19:00 +1000

    Added some basic methods for the MapGrid, changed my account to the correct one from KIN0025

--- src/main/entities/MapGrid.java
+++ src/main/entities/MapGrid.java
@@ -2,5 +2,42 @@ package main.entities;
 
 public class MapGrid {
     Intersection[][] grid;
-    int width,height;
+    int width, height;
+
+    //FIXME Not final at all.
+    public MapGrid(int width, int height) {
+        this.width = width;
+        this.height = height;
+        grid = new Intersection[width][height];
+    }
+
+    public Intersection[][] getGrid() {
+        return grid;
+    }
+
+    public boolean addIntersection(int x, int y, Intersection intersection) {
+        //FIXME Needs a hilarious amount added
+        grid[x][y] = intersection;
+        return false;
+    }
+
+    public void setGrid(Intersection[][] grid) {
+        this.grid = grid;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }

5c095f0e927e28f9e34045c048912e5690f814cf
rmit-s3630023-larry-ho <30305631+rmit-s3630023-larry-ho@users.noreply.github.com>
2017-08-24 11:17:11 +1000

    Slightly edited method names and error mesage

--- test/main/entities/intersection/TrafficLightTest.java
+++ test/main/entities/intersection/TrafficLightTest.java
@@ -22,20 +22,20 @@ public class TrafficLightTest {
     }
 
     @Test
-    public void restartCycle() throws Exception {
+    public void trafficCycle() throws Exception {
         assertEquals("Default Traffic Light State incorrect", STATUS.RED, trafficLight.getStatus());
         trafficLight.restartCycle();
         assertEquals("Reset Traffic Light State incorrect", STATUS.GREEN, trafficLight.getStatus());
         for (int i = 0; i <= 29; i++) {
             trafficLight.incrementTime();
-            assertEquals("Reset Traffic Light time incorrect", STATUS.GREEN, trafficLight.getStatus());
+            assertEquals("Traffic Light time incorrect", STATUS.GREEN, trafficLight.getStatus());
         }
         for (int i = 0; i <= 4; i++) {
             trafficLight.incrementTime();
-            assertEquals("Reset Traffic Light amber time incorrect", STATUS.AMBER, trafficLight.getStatus());
+            assertEquals("Traffic Light amber time incorrect", STATUS.AMBER, trafficLight.getStatus());
         }
         trafficLight.incrementTime();
-        assertEquals("Reset Traffic Light red time incorrect", STATUS.RED, trafficLight.getStatus());
+        assertEquals("Traffic Light red time incorrect", STATUS.RED, trafficLight.getStatus());
     }
 
     @Test

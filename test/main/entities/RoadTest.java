fa79d621f7079f4fa2b0a573283d42d9b45e361f
rmit-s3604842-adithya-gadiraju <30484133+rmit-s3604842-adithya-gadiraju@users.noreply.github.com>
2017-08-24 11:45:46 +1000

    Added road tests

---
+++ test/main/entities/RoadTest.java
@@ -0,0 +1,31 @@
+package main.entities;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RoadTest {
+
+	@Test
+	public void setUp() throws Exception {
+	}
+	
+    @Test
+    public void addIntersection() throws Exception {
+    }
+
+    @Test
+    public void getPosition() throws Exception {
+    }
+
+    @Test
+    public void getLanes() throws Exception {
+    }
+
+    @Test
+    public void getOrientation() throws Exception {
+    }
+
+    @Test
+    public void getIntersectionDirection() throws Exception {
+    }
+}
